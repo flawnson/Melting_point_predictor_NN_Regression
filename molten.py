80808520b51bc411c9dcb2c65358c4467a455244
Flawnson Tong <flawnsontong1@gmail.com>
2019-03-10 11:00:03 -0400

    Update molten.py

--- molten.py
+++ molten.py
@@ -9,6 +9,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as fun
 import torch.optim as optim
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
 
 """OPENING DATASET"""
 # Open dataset
@@ -41,8 +44,9 @@ print ("Maximum length of embedding after padding: %s" %embed)
 int_norm_smiles = []
 
 def int_and_norm(smiles_strings):
-  for smiles in smiles_strings:
-    int_norm_smiles.append([(char_to_int[char] / charset_len) for char in smiles])
+  for smiles in tqdm(smiles_strings):
+    int_norm_smiles.append([(char_to_int[char] / charset_len) for char in tqdm(smiles)])
+  return int_norm_smiles
 
 int_and_norm(smiles_strings)
 
@@ -50,9 +54,8 @@ int_and_norm(smiles_strings)
 pad_int_norm_smiles = []
 
 def pad(int_norm_smiles):
-  for smiles in int_norm_smiles:
-    diff = embed - len(smiles)
-    diff = [0] * diff
+  for smiles in tqdm(int_norm_smiles):
+    diff = (embed - len(smiles)) * [0]
     smiles = smiles + diff
     pad_int_norm_smiles.append(smiles)
   return pad_int_norm_smiles
@@ -71,7 +74,7 @@ norm_melting_points_celcius = norm(melting_points_celcius)
 X = pad_int_norm_smiles
 Y = norm_melting_points_celcius
 
-assert len(pad_int_norm_smiles) == len(norm_melting_points_celcius), "X data and Y data are not the same size"
+assert len(X) == len(Y), "X data and Y data are not the same size"
 
 """SPLITTING DATA PREPROCESSING"""
 #Split dataset into training and testing sets
@@ -81,9 +84,13 @@ def data_splitter(dataset, train_percent):
     split_length = int(train_percent * dataset_len)
     return dataset[:split_length], dataset[split_length:]
 
-X_data_train, X_data_test = data_splitter(pad_int_norm_smiles, .9)
-Y_data_train, Y_data_test = data_splitter(norm_melting_points_celcius, .9)
+X_data_train, X_data_test = data_splitter(X, .9)
+Y_data_train, Y_data_test = data_splitter(Y, .9)
 
+# Y_data_train = torch.from_numpy(Y_data_train)
+# Y_data_test = torch.from_numpy(Y_data_test)
+plt.hist(Y)
+plt.show
 # Turn datasets into tensors as input for Neural Network model
 def tensorizer(datasets):
   datasets = np.asarray(datasets)
@@ -91,25 +98,33 @@ def tensorizer(datasets):
   for dataset in datasets:
     dataset = dataset.view(-1, embed)
   return datasets
-    
+
 X_data_train = tensorizer(X_data_train)
 X_data_test = tensorizer(X_data_test)
 
-print (X_data_train)
-print (X_data_test)
+Y_data_train = torch.from_numpy(Y_data_train)
+Y_data_test = torch.from_numpy(Y_data_test)
 
-print (Y_data_train)
-print (Y_data_test)
+Y_data_train = Y_data_train.float()
+Y_data_test = Y_data_test.float()
+
+# Final check for size, length, dimensions, etc.
+if len(X_data_train) == len(Y_data_train) and len(Y_data_test) == len(Y_data_test):
+  print ("Length of training datasets is: %s" %len(X_data_train))
+  print ("Length of testing datasets is: %s" %len(X_data_test))
+else:
+  print ("Datasets to not match")
+
 """NEURAL NETWORK MODEL"""
 class Neural_Network(nn.Module):
         def __init__(self):
                 super(Neural_Network, self).__init__()
-                self.layer_1 = nn.Linear(embed, 188) # Embed is used to define numerb of input neurons (embed = length of padded smiles)
+                self.layer_1 = nn.Linear(282, 188) # Embed is used to define numerb of input neurons (embed = length of padded smiles)
                 self.layer_2 = nn.Linear(188, 94)
                 self.layer_3 = nn.Linear(94, 47)
                 self.layer_4 = nn.Linear(47, 13)
                 self.layer_5 = nn.Linear(13, 7)
-                self.layer_5 = nn.Linear(7, 1)
+                self.layer_6 = nn.Linear(7, 1)
 
                 self.sigmoid = nn.Sigmoid()
                 self.relu = nn.ReLU()
@@ -119,7 +134,8 @@ class Neural_Network(nn.Module):
                 out_2 = self.sigmoid(self.layer_2(out_1))
                 out_3 = self.sigmoid(self.layer_3(out_2))
                 out_4 = self.sigmoid(self.layer_4(out_3))
-                prediction = self.relu(self.layer_5(out_4))
+                out_5 = self.sigmoid(self.layer_5(out_4))
+                prediction = self.relu(self.layer_6(out_5))
                 return prediction
 
 # Creating a variable for our model
@@ -129,17 +145,17 @@ model = Neural_Network()
 
 # Construct loss function and optimizer
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr = 0.01)
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
 # Construct training loop
-epochs = 1000
+epochs = 100
 
 running_loss = 0.0
 for epoch in range(epochs):
 
         # Define Variables
-        prediction = model(X)
-        loss = criterion(prediction, Y)
+        prediction = model(X_data_train)
+        loss = criterion(prediction, Y_data_train)
 
         # Optimize
         optimizer.zero_grad()
@@ -151,9 +167,6 @@ for epoch in range(epochs):
         if epoch % 10 == 9:
             print('Epochs: %5d | Loss: %.3f' % (epoch + 1, running_loss / 10))
             running_loss = 0.0
-              
-"""CHECKPOINTS"""
-# # Define checkpoints (used to save the weights at each epoch, so that the model doesn't need to be retrained)
-# filepath="molten-{epoch:02d}-{loss:.4f}.hdf5"
-# checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
-# callbacks_list = [checkpoint]
+
+plt.hist(prediction)
+plt.show
